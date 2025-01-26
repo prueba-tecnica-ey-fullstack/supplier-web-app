8c3c832aacfde8cd3c036e8f5a94e82f223a7df3
renato-av <renato.avela.17@gmail.com>
2025-01-26 07:03:08 -0500

    feat: add supplier table columns with formatting and filtering options

--- src/suppliers/domain/supplier.interface.ts
+++ src/suppliers/domain/supplier.interface.ts
@@ -1,3 +1,6 @@
+import moment from 'moment'
+import { Column } from '../types'
+
 export interface Supplier {
   id: number
   name: string
@@ -17,3 +20,52 @@ export interface Supplier {
 export type SupplierDto = Omit<Supplier, 'id' | 'createdDate' | 'updatedDate'> & {
   id?: number
 }
+
+export const TableColumns: Column[] = [
+  {
+    label: 'Fecha',
+    key: 'updatedDate',
+    filterable: false,
+    render: (supplier) => moment(supplier.updatedDate).format('DD/MM/YYYY')
+  },
+  {
+    label: 'Nombre comercial',
+    key: 'name',
+    filterable: true
+  },
+  {
+    label: 'Razón Social',
+    key: 'legalName',
+    filterable: true
+  },
+  {
+    label: 'Id. Tributaria',
+    key: 'taxIdentification',
+    filterable: true
+  },
+  {
+    label: 'Teléfono',
+    key: 'phone',
+    filterable: true
+  },
+  {
+    label: 'Correo',
+    key: 'email',
+    filterable: true
+  },
+  {
+    label: 'Sitio Web',
+    key: 'website',
+    filterable: true
+  },
+  {
+    label: 'Dirección',
+    key: 'address',
+    filterable: true
+  },
+  {
+    label: 'País',
+    key: 'country',
+    filterable: true
+  }
+]
--- src/suppliers/types.ts
+++ src/suppliers/types.ts
@@ -1,4 +1,3 @@
-import moment from 'moment'
 import { Supplier } from './domain/supplier.interface'
 
 export interface Column {
@@ -8,52 +7,3 @@ export interface Column {
   render?: (proceeding: Supplier) => string
   filterable: boolean
 }
-
-export const TableColumns: Column[] = [
-  {
-    label: 'Fecha',
-    key: 'updatedDate',
-    filterable: false,
-    render: (supplier) => moment(supplier.updatedDate).format('DD/MM/YYYY')
-  },
-  {
-    label: 'Nombre comercial',
-    key: 'name',
-    filterable: true
-  },
-  {
-    label: 'Razón Social',
-    key: 'legalName',
-    filterable: true
-  },
-  {
-    label: 'Id. Tributaria',
-    key: 'taxIdentification',
-    filterable: true
-  },
-  {
-    label: 'Teléfono',
-    key: 'phone',
-    filterable: true
-  },
-  {
-    label: 'Correo',
-    key: 'email',
-    filterable: true
-  },
-  {
-    label: 'Sitio Web',
-    key: 'website',
-    filterable: true
-  },
-  {
-    label: 'Dirección',
-    key: 'address',
-    filterable: true
-  },
-  {
-    label: 'País',
-    key: 'country',
-    filterable: true
-  }
-]
